986d655652fec54c3fcaa999769025cc6532b38f
vivid20221 <julainj9011@gmail.com>
2022-11-23 02:49:25 -0600

    editing inquirer prompts

--- assets/index.js
+++ assets/index.js
@@ -1,15 +1,15 @@
-var mysql = require ('mysql2')
+var mysql = require ('mysql2');
 var inquirer = require('inquirer');
 var fs = require('fs');
 const { concat } = require('rxjs');
 
-var connection = mysql.createConnection ({
-    host:'localhost',
-    port:''
-    user:'root',
-    password:'root',
-    database:'department_db',
-})
+// var connection = mysql.createConnection ({
+//     host:'localhost',
+//     port:''
+//     user:'root',
+//     password:'root',
+//     database:'department_db',
+// })
 
 
 connection.connect(function(error){
@@ -37,6 +37,55 @@ function addDepartment(){
             message: 'What would you like to do?',
             choices: ('View All Employees', 'Add Employee', 'Update Employee Role', 'View All Roles', 'Add Role', 'View All Departments', 'Add Departments', 'Quit')
 
+
+        },
+        {
+            type:' input ',
+            name:'fistName',
+            massages:'What is the employee’s first name?',
+
+        },
+        {
+            type:'input  ',
+            name:'lastName',
+            massages:'What is the employee’s last name?',
+
+        },
+        {
+            type:'list',
+            name:'updateEmployee',
+            message:'Which employee’s role do you want to update?',
+            choices: ( 'John doe', 'Mike Chan', 'Ashley Rodriguez', 'Kevin Tupik', 'Kunal Singh', 'Malia Brown'),
+
+
+        },
+        {
+            type:'list',
+            name:'addRoles',
+            message:'What is the name of the role?',
+            choices: ('Sales Lead', 'Salesperson', 'Lead Engineer', 'Software Engineer', 'Account Manager', 'Accountant', 'Legal Team Lead', 'Lawyer', 'Customer Service'),
+        },
+        {
+            type:'list',
+            name:'managers',
+            massage:'Who is the employee’s manager?',
+            choices:('None', 'John doe', 'Mike Chan', 'Ashley Rodriguez', 'Kevin Tupik', 'Kunal Singh', 'Malia Brown'),
+        },
+        {
+            type:'input ',
+            name:'salary',
+            message:' What is the salary of the role?',
+        },
+        {
+            type:'list',
+            name:'deparmentChoice',
+            message:' Which department does the role belong to?',
+            choices:('Engineering','Finance', 'Legal', 'Sales', 'Service'),
+        },
+        {
+            type:'input ',
+            name:'addDeparments',
+            message:'What is the name of the department?',
 
         },
         
