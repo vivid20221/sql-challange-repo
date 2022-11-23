7a571c5b1897f92f24177fffa003f19c57a8f5a1
vivid20221 <julainj9011@gmail.com>
2022-11-23 00:48:52 -0600

    editing inquirer prompts

--- assets/index.js
+++ assets/index.js
@@ -1,5 +1,52 @@
-const inquirer = require('inquirer');
-const fs = require('fs');
+var mysql = require ('mysql2')
+var inquirer = require('inquirer');
+var fs = require('fs');
+const { concat } = require('rxjs');
+
+var connection = mysql.createConnection ({
+    host:'localhost',
+    port:''
+    user:'root',
+    password:'root',
+    database:'department_db',
+})
 
 
+connection.connect(function(error){
+    if(error) throw error;
+    console.log("connected at " +connection.threadId+"\n");
+    addDepartment()
 
+})
+
+function querying(){
+    connection.query("SELECT * FROM department",function(error, results){
+        if (error) throw error;
+        console.log("results",results);
+        connection.end()
+
+
+    })
+}
+
+function addDepartment(){
+    inquirer.prompt([
+        {
+            type: 'list',
+            name: 'mainMenu',
+            message: 'What would you like to do?',
+            choices: ('View All Employees', 'Add Employee', 'Update Employee Role', 'View All Roles', 'Add Role', 'View All Departments', 'Add Departments', 'Quit')
+
+
+        },
+        
+        
+
+    ]).then(function(answers){
+        console.log(answers);
+        connection.querry("INSERT INTO department SET?",{
+          mainMenu:answers.mainMenu,  
+        })
+    })
+    
+}
